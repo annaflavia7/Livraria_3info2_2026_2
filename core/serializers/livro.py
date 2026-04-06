c6398afa710355b40802d67005327a5ba01ebf7a
anaflavia7 <annaflavias369@gmail.com>
2026-04-06 16:09:03 -0300

    feat: adição do campo ManyToMany 'autores' no modelo Livro e atualização dos serializers e views correspondentes

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,9 +1,16 @@
-from core.models import Livro
-
 from rest_framework.serializers import ModelSerializer
+
+from core.models import Livro
 
 
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
+
+
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+        depth = 1
